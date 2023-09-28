84381a7481699e8c91834838ef04450ca51bb6f0
TorgunI <torgunivan@gmail.com>
2023-09-28 18:39:35 +0300

    3. Арифметические операции. [#185]

--- src/main/java/ru/job4j/Info.java
+++ src/main/java/ru/job4j/Info.java
@@ -2,6 +2,14 @@ package ru.job4j;
 
 public class Info {
     public static void main(String[] args) {
-        System.out.println("28.09.2023");
+        byte age = 22;
+        char male = 'M';
+        char female = 'F';
+        float growth = 1.85F;
+        short bricksCount = 15000;
+        int bacteriaCount = 10000000;
+        long spaceDistanceToRandomComet = 23000000000L;
+        boolean isWorkPermitted = false;
+        byte fileSize = 122;
     }
 }
