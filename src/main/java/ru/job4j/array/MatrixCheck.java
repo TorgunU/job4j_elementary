cabcda652763aab2f3498e829a7e33cb541fa753
TorgunI <torgunivan@gmail.com>
2023-10-05 20:05:03 +0300

    6.7.4. Выигрышные комбинации в сокобан [#53859]

--- src/main/java/ru/job4j/array/MatrixCheck.java
+++ src/main/java/ru/job4j/array/MatrixCheck.java
@@ -1,10 +1,10 @@
 package ru.job4j.array;
 
 public class MatrixCheck {
-    public static boolean monoHorizontal(char[][] boad, int row) {
+    public static boolean monoHorizontal(char[][] board, int row) {
         boolean result = true;
-        for (int i = 0; i < boad[row].length; i++) {
-            if (boad[row][i] != 'X') {
+        for (int i = 0; i < board[row].length; i++) {
+            if (board[row][i] != 'X') {
                 result = false;
                 break;
             }
@@ -27,6 +27,18 @@ public class MatrixCheck {
         char[] result = new char[board.length];
         for (int i = 0; i < board.length; i++) {
             result[i] = board[i][i];
+        }
+        return result;
+    }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int i = 0; i < board.length; i++) {
+            if (board[i][i] == 'X'
+                    && monoHorizontal(board, i) || monoVertical(board, i)) {
+                result = true;
+                break;
+            }
         }
         return result;
     }
