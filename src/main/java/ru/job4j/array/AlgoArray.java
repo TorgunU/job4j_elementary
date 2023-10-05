df28cb6ea0cf04e393212842397663902153f32c
TorgunI <torgunivan@gmail.com>
2023-10-05 16:27:03 +0300

    6.0.6. Заполнить массив степенями чисел. [#33488 #450188]

--- src/main/java/ru/job4j/array/AlgoArray.java
+++ src/main/java/ru/job4j/array/AlgoArray.java
@@ -3,17 +3,18 @@ package ru.job4j.array;
 public class AlgoArray {
     public static void main(String[] args) {
         int[] array = new int[] {5, 3, 2, 1, 4};
-        int temp;
 
-        for (int i = 0; i < array.length; i++) {
-            for (int j = i; j < array.length; j++) {
-                if (array[i] > array[j]) {
-                    temp = array[i];
-                    array[i] = array[j];
-                    array[j] = temp;
-                }
-            }
-        }
+        int temp = array[0];
+        array[0] = array[3];
+        array[3] = temp;
+
+        temp = array[1];
+        array[1] = array[2];
+        array[2] = temp;
+
+        temp = array[4];
+        array[4] = array[3];
+        array[3] = temp;
 
         for (int number : array) {
             System.out.println(number);
